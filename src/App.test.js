e5bddc0156699cd0896f4fa94fbab091b17c2398
warthex <yonathanw@outlook.es>
2021-02-12 14:45:13 -0500

    test ok

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import axios from 'axios';
+import ReportService from "./service/ReportService";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+ 
+jest.mock('axios')
+
+test('returned observable with request post', () => {
+  const add = {
+    technicalId: 'wtx-1',
+    serviceId: 's-1',
+    startDate: '2021-01-20T07:00',
+    endDate: '2021-01-20T07:10',
+    weekNumber:4
+}
+  axios.post.mockResolvedValue(add);
+  return  axios.post(add).then(data => expect(data).toEqual(add));
+}); 
+
+test('returned calculate hours worked find by technical and number week from api via ge', () => {
+  const hourReport = {
+    normalHours: 26.0,
+    nightHours: 22.0,
+    sundayHours: 0.0,
+    normalExtraHours: 0.0,
+    nightlyOvertime: 11.0,
+    sundayExtraHours: 13.0
+  }
+const technical='wtx-1'
+const weekNumber=4
+
+  axios.get.mockResolvedValue(hourReport);
+  return  axios.get(technical,weekNumber).then(data => expect(data).toEqual(hourReport));
+}); 
