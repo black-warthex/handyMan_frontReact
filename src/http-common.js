e7094458573b3301aba1b11b0a5d9dae8db978b8
warthex <yonathanw@outlook.es>
2021-02-15 11:59:54 -0500

    other

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+
+
 export default axios.create({
-  baseURL: "http://localhost:8080/report/",
+  baseURL: "http://ec2-3-137-195-184.us-east-2.compute.amazonaws.com/report/",
   headers: {
     "Content-type": "application/json",
     "Accept":"application/json"
   }
 })
+
+//baseURL: "http://3.137.195.184/report/",
